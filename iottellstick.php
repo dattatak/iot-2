441fcb7c85148431c4d1b933a576b38f8ddb7e79
epideme <tony.nilsson@epideme.com>
2016-08-18 07:57:35 +0200

    Test new method for a more linear switching with daemon

--- iottellstick.php
+++ iottellstick.php
@@ -8,24 +8,25 @@ if (isset($_GET['id'])){
 		  if ($got == 'ON') {$final = '100';}
 	  	if ($got == 'OFF') {$final = '0';}
 		  if ((floatval($got) > 0) && (floatval($got) < 255)) {$final = round(floatval($got)/255*100);}
-		  } 
+		  }
 		} else {
 		if ($got == 'ON') {$final = 'on';}
                 if ($got == 'OFF') {$final = 'off';}
 		}
 	if (isset($_GET['set'])) {
+		$filename = $microtime . ".tellstickrun";
 		$setmode = $_GET['set'];
 		if ($_GET['set'] == 'ON') {$setmode = '100';}
                 if ($_GET['set'] == 'OFF') {$setmode = '0';}
 		if (floatval($setmode) < 2) {
-			 $final = `tdtool --off {$_GET['id']}; tdtool --off {$_GET['id']};`;
+			 $final = `echo "tdtool --off {$_GET['id']}" > $filename`;
 			} elseif (floatval($setmode) > 98) {
-			 $final = `tdtool --on {$_GET['id']}; tdtool --on {$_GET['id']};`;
+			 $final = `echo "tdtool --on {$_GET['id']}"> $filename`;
 			} else {
 				$value = round(floatval($_GET['set'])/100*255);
-				$final = `tdtool --dimlevel $value --dim {$_GET['id']}; tdtool --dimlevel $value --dim {$_GET['id']};`;
+				$final = `echo "tdtool --dimlevel $value --dim {$_GET['id']}" > $filename`;
 			}
-		system("tdtool --list-devices > $myhomedir/tddevices.txt");
+		#system("tdtool --list-devices > $myhomedir/tddevices.txt");
 		}
 	print $final;
 	} else {
