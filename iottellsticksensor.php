e6c8361c1d6611fe6dcbd34054a9c0b00e4e58c1
epideme <tony.nilsson@epideme.com>
2016-10-13 22:50:11 +0200

    Variable typo

--- iottellsticksensor.php
+++ iottellsticksensor.php
@@ -4,9 +4,9 @@ include "settings-iot.php";
 if (isset($_GET['id'])){
 	if (isset($_GET['type'])) {
 	  if ($_GET['type'] == "temp") {
-			$got = trim(`tdtool --list-sensors | grep id={$_GET['id']}[[:space:]] | awk -F '=' '{print \$10}'`);
+			$final = trim(`tdtool --list-sensors | grep id={$_GET['id']}[[:space:]] | awk -F '=' '{print \$10}'`);
 		} elseif ($_GET['type'] == "hum") {
-			$got = trim(`tdtool --list-sensors | grep id={$_GET['id']}[[:space:]] | awk -F '=' '{print \$12}'`);
+			$final = trim(`tdtool --list-sensors | grep id={$_GET['id']}[[:space:]] | awk -F '=' '{print \$12}'`);
 			}
 		}
 	print $final;
