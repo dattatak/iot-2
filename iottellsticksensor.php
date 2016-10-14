f412b147ad01bc8c72a4ff81dc41f4f49990ead9
epideme <tony.nilsson@epideme.com>
2016-10-14 10:20:26 +0200

    Update iottellsticksensor.php

--- iottellsticksensor.php
+++ iottellsticksensor.php
@@ -4,9 +4,9 @@ include "settings-iot.php";
 if (isset($_GET['id'])){
 	if (isset($_GET['type'])) {
 	  if ($_GET['type'] == "temp") {
-			$final = trim(`tdtool --list-sensors | grep id={$_GET['id']} | awk -F'[=\\t]' '{print \$10}'`);
+			$final = trim(`cat $myhomedir/tdsensors.txt | grep id={$_GET['id']} | awk -F'[=\\t]' '{print \$10}'`);
 		} elseif ($_GET['type'] == "hum") {
-			$final = trim(`tdtool --list-sensors | grep id={$_GET['id']} | awk -F'[=\\t]' '{print \$12}'`);
+			$final = trim(`cat $myhomedir/tdsensors.txt | grep id={$_GET['id']} | awk -F'[=\\t]' '{print \$12}'`);
 			}
 		}
 	print $final;
